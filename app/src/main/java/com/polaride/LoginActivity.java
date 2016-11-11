59f5e43bd19ecc46c934100231bc7bb5d5dee0fb
chunkiet66 <kiet5629@hotmail.com>
2016-11-11 10:43:12 -0800

    added facebook login

--- app/src/main/java/com/polaride/LoginActivity.java
+++ app/src/main/java/com/polaride/LoginActivity.java
@@ -2,6 +2,7 @@ package com.polaride;
 
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +12,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
@@ -20,15 +20,36 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.gson.Gson;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class LoginActivity extends AppCompatActivity {
+    public class LoginPOJO {
+
+        private String loginMethod;
+        private String userId;
+        private String userToken;
+
+        public String getLoginMethod() { return this.loginMethod; }
+        public void setLoginMethod( String loginMethod ) { this.loginMethod = loginMethod; }
+
+        public String getUserId() { return this.userId; }
+        public void setUserId( String userId ) { this.userId = userId; }
+
+        public String getUserToken() { return this.userToken; }
+        public void setUserToken( String userToken ) { this.userToken = userToken; }
+
+    }
 
     private static Context context;
     static CallbackManager mFacebookCallbackManager;
     static LoginButton mFacebookSignInButton;
     static AccessToken mFacebookAccessToken;
+
+    LoginPOJO loginPOJO = new LoginPOJO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +71,11 @@ public class LoginActivity extends AppCompatActivity {
                         //TODO: Use the Profile class to get information about the current user.
                         mFacebookAccessToken = loginResult.getAccessToken();
 
-                        Log.d( "token", mFacebookAccessToken.getToken());
-                        Log.d( "userID", mFacebookAccessToken.getUserId());
+                        loginPOJO.setLoginMethod("facebook");
+                        loginPOJO.setUserId(mFacebookAccessToken.getUserId());
+                        loginPOJO.setUserToken(mFacebookAccessToken.getToken());
+
+                        sendFacebookTokenToServer(loginPOJO);
                     }
 
                     @Override
@@ -79,24 +103,34 @@ public class LoginActivity extends AppCompatActivity {
         return LoginActivity.context;
     }
 
-    static void sendFacebookTokenToServer() {
+    static void sendFacebookTokenToServer(LoginPOJO loginPOJO) {
         RequestQueue queue = Volley.newRequestQueue(getAppContext());
-        String url = "https://private-861e1e-remoraapi.apiary-mock.com/mobile/user";
+        String url = "http://private-861e1e-remoraapi.apiary-mock.com/user/mobile/login";
 
-        JSONObject jsonObject = new JSONObject();
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("Come on here", "got it");
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("That didnt work!", "very well");
-                    }
-        });
+        Gson gson = new Gson();
+        String loginJSON = gson.toJson(loginPOJO);
+        JSONObject jsonObj;
 
-        queue.add(jsonRequest);
+        try {
+            jsonObj = new JSONObject(loginJSON);
+            Log.d("json_object", jsonObj.toString());
+            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObj,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("json_response", response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("ERROR", "I got an error", error);
+                }
+            });
+
+            queue.add(jsonRequest);
+        } catch (JSONException e) {
+            Log.e("ERROR", "I got an error", e);
+        }
+
     }
 }
